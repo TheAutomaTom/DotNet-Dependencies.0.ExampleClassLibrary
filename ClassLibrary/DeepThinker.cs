e7d866d65c3eb3396ba4d1506670ec8bb6143c12
Tom Grossi <TheAutomaTom@gmail.com>
2024-03-25 22:50:09 -0500

    Demo Library Update

--- ClassLibrary/DeepThinker.cs
+++ ClassLibrary/DeepThinker.cs
@@ -3,7 +3,7 @@
   /// <summary> Provides everyone an answer. </summary>
   public static class DeepThinker
   {
-    public readonly static string TheAnswer = "Your wish is granted!"; 
+    public readonly static string TheAnswer = "The answer is blowing in the wind"; 
   }
 
 }
